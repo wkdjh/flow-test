4e6a5d8aa4e14c41c5f07ade476c4ed51365a436
wkdjh <wkdjh1019@gmail.com>
2025-05-14 18:35:48 +0900

    add feature : Get, Post

---
+++ src/main/java/app/repository/EmpRepository.java
@@ -0,0 +1,13 @@
+package app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import app.entity.Emp;
+
+
+@Repository
+public interface EmpRepository extends JpaRepository<Emp, Integer>{
+
+}
+
